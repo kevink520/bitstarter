9ae2e5c87e45b5b10ddfbdd727a53e42cd50ddfe
Kevin K <kevinkim75@gmail.com>
2013-07-24 06:02:15 +0000

    Increased paragraph line height.

--- grader.js
+++ grader.js
@@ -22,10 +22,13 @@ References:
 */
 
 var fs = require('fs');
+var sys = require('util');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://immense-reaches-2063.herokuapp.com/";
 
 var assertFileExists = function(inFile) {
     var instr = inFile.toString();
@@ -65,8 +68,22 @@ if (require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u --url <url>', 'URL', URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    var input;
+    if (program.file) {
+	input = program.file;
+    } else if (program.url) {
+	rest.get(program.url).on('complete', function(result) {
+	    if (result instanceof Error) {
+		sys.puts('Error: ' + result.message);
+		this.retry(5000); // try again after 5 sec 
+	    } else {
+		input = result;
+	    }
+	});   
+    }
+    var checkJson = checkHtmlFile(input, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
